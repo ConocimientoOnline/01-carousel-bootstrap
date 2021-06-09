00084aa7758d94f45c27dd5d3877d81f939505c4
ArtCon <conocimientoonline36@gmail.com>
2021-06-09 15:47:43 +0200

    Commit-01
    Commit-01

---
+++ carousel-bootstrap/js.js
@@ -0,0 +1,65 @@
+function desplazar_tapa_img1(){
+	var tapa = document.getElementById('tapa_img1');
+	tapa.style.top = 0;
+}
+function quitar_tapa_img1(){
+	var tapa = document.getElementById('tapa_img1');
+	tapa.style.top = "-120%";
+}
+function desplazar_tapa_img2(){
+	var tapa = document.getElementById('tapa_img2');
+	tapa.style.top = 0;
+}
+function quitar_tapa_img2(){
+	var tapa = document.getElementById('tapa_img2');
+	tapa.style.top = "-120%";
+}
+function desplazar_tapa_img3(){
+	var tapa = document.getElementById('tapa_img3');
+	tapa.style.top = 0;
+}
+function quitar_tapa_img3(){
+	var tapa = document.getElementById('tapa_img3');
+	tapa.style.top = "-120%";
+}
+function desplazar_tapa_img4(){
+	var tapa = document.getElementById('tapa_img4');
+	tapa.style.top = 0;
+}
+function quitar_tapa_img4(){
+	var tapa = document.getElementById('tapa_img4');
+	tapa.style.top = "-120%";
+}
+
+function mostrar_imagen_grande01(){
+	var img = document.getElementById('img1');
+	img.style.top = 0;
+}
+function ocultar_imagen_grande01(){
+	var img = document.getElementById('img1');
+	img.style.top = "-120%";
+}
+function mostrar_imagen_grande02(){
+	var img = document.getElementById('img2');
+	img.style.top = 0;
+}
+function ocultar_imagen_grande02(){
+	var img = document.getElementById('img2');
+	img.style.top = "-120%";
+}
+function mostrar_imagen_grande03(){
+	var img = document.getElementById('img3');
+	img.style.top = 0;
+}
+function ocultar_imagen_grande03(){
+	var img = document.getElementById('img3');
+	img.style.top = "-120%";
+}
+function mostrar_imagen_grande04(){
+	var img = document.getElementById('img4');
+	img.style.top = 0;
+}
+function ocultar_imagen_grande04(){
+	var img = document.getElementById('img4');
+	img.style.top = "-120%";
+}
